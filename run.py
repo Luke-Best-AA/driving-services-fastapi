1b734f6728a5fad6af679912bb70f4aa8b07dd97
Luke Best <145115698+Luke-Best-AA@users.noreply.github.com>
2025-05-29 19:39:14 +0100

    made run.py environment aware

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 from app.main import app
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    env = os.getenv("ENV", "dev")
+    host = os.getenv("HOST", "0.0.0.0" if env == "prod" else "127.0.0.1")
+    port = int(os.getenv("PORT", 8000))
+    reload = env != "prod"
+    uvicorn.run("app.main:app", host=host, port=port, reload=reload)
